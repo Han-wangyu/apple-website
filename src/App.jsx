058e919c4cf09382c2893d843ed756258d963595
hanwangyu <hanwangyu@foxmail.com>
2024-03-18 16:18:25 +0800

    feat: defination of Hero, Hightlights and Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import Hightlights from './components/Hightlights'
+
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <main className="bg-black">
+      <Navbar />
+      <Hero />
+      <Hightlights />
+    </main>
   )
 }
