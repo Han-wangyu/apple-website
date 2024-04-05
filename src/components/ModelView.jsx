06e13b7c689cebe06cb5bcb759fcb29900b8cc8c
hanwangyu <hanwangyu@foxmail.com>
2024-04-05 20:53:40 +0800

    feat: better loader for 3D model

--- src/components/ModelView.jsx
+++ src/components/ModelView.jsx
@@ -2,7 +2,7 @@ import { Html, OrbitControls, PerspectiveCamera, View } from "@react-three/drei"
 
 import * as THREE from 'three'
 import Lights from './Lights';
-// import Loader from './Loader';
+import Loader from './Loader';
 import IPhone from './IPhone';
 import { Suspense } from "react";
 
@@ -31,7 +31,7 @@ const ModelView = ({ index, groupRef, gsapType, controlRef, setRotationState, si
       /> 
 
       <group ref={groupRef} name={`${index === 1} ? 'small' : 'large`} position={[0, 0 ,0]}>
-        <Suspense fallback={<Html><div>Loading</div></Html>}>
+        <Suspense fallback={<Loader />}>
           <IPhone 
             scale={index === 1 ? [15, 15, 15] : [17, 17, 17]}
             item={item}
